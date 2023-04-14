03469356f5255ad7c5ebd3bc1ebeccb092d7059d
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-04-13 21:15:23 -0300

    remove: removing all files js to can put in ts

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header() {
-  return (
-    <h1>Project StarWars</h1>
-  );
-}
-
-export default Header;
--- src/services/title.js
+++
@@ -1,4 +0,0 @@
-export default function title(str) {
-  return str.replace(/\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
-}
