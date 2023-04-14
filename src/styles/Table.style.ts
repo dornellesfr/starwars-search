87d993d340da625e4e443d1282c892087f3a5688
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-04-13 23:32:19 -0300

    style: table component styled

---
+++ src/styles/Table.style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const StyledTable = styled.div`
+  border-collapse: collapse;
+  margin: 25px 20px;
+  font-size: 0.9rem;
+  max-height: 69vh;
+  border-radius: 5px 5px 0 0;
+  overflow: auto;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+  & thead tr {
+    background-color: black;
+    color: #fbe61a;
+    text-align: center;
+  }
+
+  & th {
+    padding: 2rem;
+  }
+
+  & tbody tr {
+    border-bottom: 1px solid #ddd;
+  }
+
+  & tbody td {
+    padding: 15px 20px;
+    text-align: center;
+  }
+
+  & tbody tr:nth-child(even) {
+    background-color: #f3f3f3;
+  }
+
+  & tbody tr:last-child {
+    border-bottom: 4px solid black;
+  }
+`;
+
+export default StyledTable;
