2fe829e908c75ddfa0b2a9df1ec83be1fac77b12
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-04-13 23:31:38 -0300

    feat: Header component styled

---
+++ src/styles/Header.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  height: 10vh;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+export default Header;
